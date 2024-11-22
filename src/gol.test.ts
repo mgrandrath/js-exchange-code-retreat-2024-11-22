5d0a9df463c58fe7bc2b9ec63f84384c796cffb3
Martin Grandrath <martin.grandrath@codecentric.de>
2024-11-22 11:04:35 +0100

    chore: mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/gol.test.ts

--- src/gol.test.ts
+++ src/gol.test.ts
@@ -1,6 +1,32 @@
 import { describe, expect, it } from "vitest";
 
 describe("GOL rules", () => {
+  it.each`
+    cellState  | neighbours | expectedCellState
+    ${"ALIVE"} | ${0}       | ${"DEAD"}
+    ${"ALIVE"} | ${1}       | ${"DEAD"}
+    ${"ALIVE"} | ${2}       | ${"ALIVE"}
+    ${"ALIVE"} | ${3}       | ${"ALIVE"}
+    ${"ALIVE"} | ${4}       | ${"DEAD"}
+    ${"ALIVE"} | ${5}       | ${"DEAD"}
+    ${"ALIVE"} | ${6}       | ${"DEAD"}
+    ${"ALIVE"} | ${7}       | ${"DEAD"}
+    ${"ALIVE"} | ${8}       | ${"DEAD"}
+    ${"DEAD"}  | ${0}       | ${"DEAD"}
+    ${"DEAD"}  | ${1}       | ${"DEAD"}
+    ${"DEAD"}  | ${2}       | ${"DEAD"}
+    ${"DEAD"}  | ${3}       | ${"ALIVE"}
+    ${"DEAD"}  | ${4}       | ${"DEAD"}
+    ${"DEAD"}  | ${5}       | ${"DEAD"}
+    ${"DEAD"}  | ${6}       | ${"DEAD"}
+    ${"DEAD"}  | ${7}       | ${"DEAD"}
+    ${"DEAD"}  | ${8}       | ${"DEAD"}
+  `("test %", ({ cellState, neighbours, expectedCellState }) => {
+    const next = evolveCell(cellState, neighbours);
+
+    expect(next).toEqual(expectedCellState);
+  });
+
   it("cell dies because of under population", () => {
     const next = evolveCell("ALIVE", 0);
 
@@ -30,19 +56,24 @@ describe("GOL rules", () => {
 
     expect(next).toEqual("DEAD");
   });
+
+  it("three cells reproduce", () => {
+    const next = evolveCell("DEAD", 3);
+
+    expect(next).toEqual("ALIVE");
+  });
 });
 
 type CellState = "ALIVE" | "DEAD";
-
 
 const evolveCell = (cellState: CellState, livingNeighbours: number) => {
   switch (livingNeighbours) {
     case 1:
       return "DEAD";
-    case 2:
-      case 3:
-        return "ALIVE"
 
+    case 2:
+    case 3:
+      return "ALIVE";
   }
   return livingNeighbours === 2 ? "ALIVE" : "DEAD";
 };
