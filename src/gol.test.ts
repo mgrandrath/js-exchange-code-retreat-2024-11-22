c2a6db172aad261fd74bf1c998877a8674b582ea
Martin Grandrath <martin.grandrath@codecentric.de>
2024-11-22 10:50:05 +0100

    chore: mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/gol.test.ts

--- src/gol.test.ts
+++ src/gol.test.ts
@@ -2,11 +2,16 @@ import { describe, expect, it } from "vitest";
 
 describe("GOL rules", () => {
   it("cell dies because of under population", () => {
-    const next = evolveCell("ALIVE", 2);
+    const next = evolveCell("ALIVE", 1);
 
     expect(next).toEqual("DEAD");
   });
+
+  it("survives", () => {
+    const next = evolveCell("ALIVE", 2);
+
+    expect(next).toEqual("ALIVE");
+  });
 });
 
-
-const evolveCell = ()=>"DEAD";
+const evolveCell = () => "DEAD";
