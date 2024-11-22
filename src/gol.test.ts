d53ea92b3bc0a842f0cde69a26b866f5bc9951ce
Martin Grandrath <martin.grandrath@codecentric.de>
2024-11-22 10:54:03 +0100

    chore: mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/gol.test.ts

--- src/gol.test.ts
+++ src/gol.test.ts
@@ -2,18 +2,36 @@ import { describe, expect, it } from "vitest";
 
 describe("GOL rules", () => {
   it("cell dies because of under population", () => {
+    const next = evolveCell("ALIVE", 0);
+
+    expect(next).toEqual("DEAD");
+  });
+
+  it("cell dies because of under population", () => {
     const next = evolveCell("ALIVE", 1);
 
     expect(next).toEqual("DEAD");
   });
 
-  it("survives", () => {
+  it("survives with two neighbours", () => {
     const next = evolveCell("ALIVE", 2);
 
     expect(next).toEqual("ALIVE");
   });
+
+  it("survives with three neighbours", () => {
+    const next = evolveCell("ALIVE", 3);
+
+    expect(next).toEqual("ALIVE");
+  });
+
+  it("dies because of overcrowding", () => {
+    const next = evolveCell("ALIVE", 4);
+
+    expect(next).toEqual("DEAD");
+  });
 });
 
 const evolveCell = (_, livingNeighbours) => {
-  return livingNeighbours === 2 ? "ALIVE": "DEAD";
+  return livingNeighbours === 2 ? "ALIVE" : "DEAD";
 };
